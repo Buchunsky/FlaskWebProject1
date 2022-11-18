8d265e495324d42ffc2186257c92acfb790ae0b4
Alina Horkovaya <horkovayalina@gmail.com>
2022-11-18 12:50:19 +0200

    Add project files.

---
+++ FlaskWebProject1/views.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from flask import render_template, Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+app.run(port=5000)
